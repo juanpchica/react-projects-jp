f1456fa6d18c68179f5c63b84e06ba99037c8b73
juanpchica <juanpchica@hotmail.com>
2021-01-19 22:21:09 -0500

    useState and useEffect added to this example birthdayReminder

--- src/pages/simple/BirthdayReminder.js
+++ src/pages/simple/BirthdayReminder.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 import { data } from "../../pages/data";
 const BirthdayReminder = () => {
+  const [people, setPeople] = useState(data);
+
+  useEffect(() => {
+    console.log("component rendered");
+  });
   return (
     <React.Fragment>
       <section className="container">
         <ul className="list-group mt-5">
-          {data.map((person) => {
+          {people.map((person) => {
             return (
               <li className="list-group-item" key={person.id}>
                 <h4>{person.name}</h4>
@@ -18,6 +24,14 @@ const BirthdayReminder = () => {
             );
           })}
         </ul>
+
+        <button
+          className="btn btn-success btn-large mt-5"
+          type="button"
+          onClick={() => setPeople([])}
+        >
+          Clear All
+        </button>
       </section>
     </React.Fragment>
   );
