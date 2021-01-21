3576555d41409734ce706539f69f1bf4ad495616
juanpchica <juanpchica@hotmail.com>
2021-01-20 23:17:57 -0500

    Css rules added with Separate components added

--- src/pages/simple/Tours.js
+++ src/pages/simple/Tours.js
@@ -1,41 +1,59 @@
 import React, { useState, useEffect } from "react";
+import "../simple/Tour.css";
 
 const Tours = () => {
   const url = "https://course-api.com/react-tours-project";
   const [tours, setTours] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const getData = async () => {
     const response = await fetch(url);
     const tours = await response.json();
     setTours(tours);
+    setLoading(false);
   };
 
   useEffect(() => {
     getData();
   }, []);
 
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
-    <section>
-      <ul>
-        {tours.map((tour) => {
-          <li>
-            <Tour tour={tour} />
-          </li>;
-        })}
-      </ul>
-    </section>
+    <main>
+      {tours.map((tour) => {
+        return <Tour key={tour.id} {...tour} />;
+      })}
+    </main>
   );
 };
 
-const Tour = (props) => {
-  console.log(props);
-  const { name, info, image, price } = props.tour;
+const Tour = ({ name, info, image, price }) => {
+  const [readMore, setReadMore] = useState(false);
   return (
-    <>
-      <img src={image} />
-      <h2>{name}</h2>
-    </>
+    <article className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? "show less" : "  read more"}
+          </button>
+        </p>
+        <button className="delete-btn">not interested</button>
+      </footer>
+    </article>
   );
+};
+
+const Loading = () => {
+  return <h2>Loading...</h2>;
 };
 
 export default Tours;
