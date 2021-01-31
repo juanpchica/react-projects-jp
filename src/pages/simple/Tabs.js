d894d16d5c4e76991e50c48be4a703097bdba58e
juanpchica <juanpchica@hotmail.com>
2021-01-31 10:54:37 -0500

    Data fetch with stringify

--- src/pages/simple/Tabs.js
+++ src/pages/simple/Tabs.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Tabs = () => {
-  return <div>hi</div>;
+  const url = "https://course-api.com/react-tabs-project";
+  const [info, setInfo] = useState("");
+  const getData = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    setInfo(JSON.stringify(data));
+  };
+
+  useEffect(() => {
+    console.log("render is ready");
+    getData();
+  }, []);
+
+  return <div>{info}</div>;
 };
 
 export default Tabs;
