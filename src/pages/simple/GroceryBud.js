7a5c625d80c5057be13aa39751ffa0349b84d8c3
juanpchica <juanpchica@hotmail.com>
2021-02-10 22:37:28 -0500

    List added and mapping with validation of new components

--- src/pages/simple/GroceryBud.js
+++ src/pages/simple/GroceryBud.js
@@ -4,15 +4,23 @@ const GroceryBud = () => {
   const [name, setName] = useState("");
   const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    alert("works");
+    if (!name) {
+    } else if (name && isEditing) {
+    } else {
+      const newItem = { id: new Date().getTime().toString(), name: name };
+      setList([...list, newItem]);
+      setName("");
+    }
   };
 
   return (
     <section>
       <form onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
         <input
           type="text"
           className="form-control"
@@ -24,7 +32,25 @@ const GroceryBud = () => {
           {isEditing ? "Update" : "Submit"}
         </button>
       </form>
+
+      <List list={list} />
     </section>
+  );
+};
+
+const Alert = () => {
+  return <section>alert</section>;
+};
+
+const List = (props) => {
+  const list = props.list;
+
+  return (
+    <ul>
+      {list.map((item) => {
+        return <li key={item.id}>{item.name}</li>;
+      })}
+    </ul>
   );
 };
 
