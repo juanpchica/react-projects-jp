a06093e1593b1a099543622e409ef48e06fe88e3
juanpchica <juanpchica@hotmail.com>
2021-02-10 22:21:52 -0500

    New list component added

--- src/pages/simple/GroceryBud.js
+++ src/pages/simple/GroceryBud.js
@@ -1,56 +1,30 @@
 import React, { useState } from "react";
 
 const GroceryBud = () => {
-  const [groceryItems, setGroceryItems] = useState([]);
-  const [idItem, setIdItem] = useState(0);
-  const addItem = (e, name) => {
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    setIdItem(idItem++);
-    setGroceryItems([...groceryItems, { id: idItem, name: name }]);
+    alert("works");
   };
+
   return (
     <section>
-      <h1>Grocery Bud</h1>
-      <Searcher addItem={addItem} />
-      <List groceryItems={groceryItems} />
-    </section>
-  );
-};
-
-const Searcher = (props) => {
-  const { addItem } = props;
-  const [name, setName] = useState("");
-  return (
-    <div>
-      <form
-        onSubmit={(e) => {
-          addItem(e, name);
-        }}
-      >
-        <div className="form-group">
-          <input
-            type="text"
-            className="form-control"
-            value={name}
-            onChange={(e) => {
-              setName(e.target.value);
-            }}
-          />
-          <input type="submit" value="Submit" />
-        </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="e.g eggs"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button type="submit" className="btn btn-default">
+          {isEditing ? "Update" : "Submit"}
+        </button>
       </form>
-    </div>
-  );
-};
-
-const List = ({ groceryItems }) => {
-  console.log(groceryItems);
-  return (
-    <ul>
-      {groceryItems.map((item, i) => {
-        return <li key={i}>{item}</li>;
-      })}
-    </ul>
+    </section>
   );
 };
 
