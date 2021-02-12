b6f1566aa621cc1987b737c441d975f7271b2137
juanpchica <juanpchica@hotmail.com>
2021-02-11 22:34:34 -0500

    Use ref for menu height added

--- src/pages/simple/NavbarIn.js
+++ src/pages/simple/NavbarIn.js
@@ -55,18 +55,30 @@ const social = [
 
 const NavbarIn = () => {
   const [showLinks, setShowLinks] = useState(false);
-
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src="" className="logo" alt="logo" />
-          <button className="nav-toggle">
+          <button className="nav-toggle" onClick={toggleLinks}>
             <FaBars />
           </button>
         </div>
-        <div className="links-container">
-          <ul className="links">
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
             {links.map((link) => {
               const { id, url, text } = link;
               return (
